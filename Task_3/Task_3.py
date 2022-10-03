4f88f9b8b073eb634c6bab91cbcbd0edbd246a10
Лилия <l_loshakova123@mail.ru>
2022-10-03 23:52:28 +0300

    Outline of the solution of task 3

--- Task_3/Task_3.py
+++ Task_3/Task_3.py
@@ -1,4 +1,11 @@
-print('Enter the number a: ')
+print('Enter a negative number a: ')
 a = int(input())
 print('Enter the number b: ')
 b = int(input())
+
+
+
+if (a <= b):
+    f = a + 1
+else:
+    print(f)
