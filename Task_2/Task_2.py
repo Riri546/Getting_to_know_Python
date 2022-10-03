b8d5921d605f71d34294911d6fd42756c304534c
Лилия <l_loshakova123@mail.ru>
2022-10-03 23:35:03 +0300

    Solution Task 2

---
+++ Task_2/Task_2.py
@@ -0,0 +1,39 @@
+print('Enter the number a: ')
+a = int(input())
+print('Enter the number b: ')
+b = int(input())
+print('Enter the number c: ')
+c = int(input())
+print('Enter the number d: ')
+d = int(input())
+print('Enter the number e: ')
+e = int(input())
+
+max = 0
+
+if (max > a):
+    max
+else:
+    max = a
+
+if (max > b):
+    max
+else:
+    max = b
+
+if (max > c):
+    max
+else:
+    max = c
+
+if (max > d):
+    max
+else:
+    max = d
+
+if (max > e):
+    max
+else:
+    max = e
+
+print(f'Max = {max}')
