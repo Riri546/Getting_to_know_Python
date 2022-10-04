956328caf8da213b495d8ca14f75a49b01708e50
Лилия <l_loshakova123@mail.ru>
2022-10-04 22:33:22 +0300

    Commit for task 9

--- Task_9.py
+++ Task_9.py
@@ -1,2 +1,2 @@
 # 9. By specifying the number of the quarter of the rectangular coordinate system, show
-# the acceptable coordinate values for the points of this quarter.
+# the acceptable coordinate values for the points of this quarter
