f3f8d5c76ea9cd5d481b4e4fd70b6f8b2114c74f
Лилия <l_loshakova123@mail.ru>
2022-10-05 00:29:25 +0300

    Outline of the solution

--- Homework_1/Hw_5.py
+++ Homework_1/Hw_5.py
@@ -1,1 +1,47 @@
 # Write a program that takes the coordinates of two points as input and finds the distance between them in 2D space.
+
+
+
+
+
+
+# Console.Clear();
+
+# int coordXPointA;
+# int coordXPointB;
+# int coordYPointA;
+# int coordYPointB;
+
+# double lengthAB;
+
+# //Reads coordinates of points A and B
+# void ReadDataOfPoint()
+# {
+#     Console.Write("Enter the X coordinate of point A: ");
+#     string inputNum1 = Console.ReadLine() ?? "";
+#     coordXPointA = int.Parse(inputNum1);
+
+#     Console.Write("Enter the Y coordinate of point A: ");
+#     string inputNum2 = Console.ReadLine() ?? "";
+#     coordYPointA = int.Parse(inputNum2);
+
+#     Console.Write("Enter the X coordinate of point B: ");
+#     string inputNum4 = Console.ReadLine() ?? "";
+#     coordXPointB = int.Parse(inputNum4);
+
+#     Console.Write("Enter the Y coordinate of point B: ");
+#     string inputNum5 = Console.ReadLine() ?? "";
+#     coordYPointB = int.Parse(inputNum5);
+# }
+
+
+# //Calculates the distance between points A and B
+# void ConculateLateLengthAB()
+# {
+#     lengthAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB), 2) + Math.Pow((coordYPointA - coordYPointB), 2));
+# }
+
+# ReadDataOfPoint();
+# ConculateLateLengthAB();
+
+# Console.WriteLine(lengthAB);
