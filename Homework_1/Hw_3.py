f39cc7d18020655107c27ce7fbe4518ffe39f773
Лилия <l_loshakova123@mail.ru>
2022-10-05 00:22:57 +0300

    Commit for homework 3

---
+++ Homework_1/Hw_3.py
@@ -0,0 +1,2 @@
+# Write a program that takes the coordinates of a point (X and Y) as input, with X ≠ 0 and Y ≠ 0 and
+#  outputs the number of the quarter of the plane in which this point is located (or on which axis it is located).
