592d6fee30ed0826f500658d39fcb46b0fc6cdf0
Лилия <l_loshakova123@mail.ru>
2022-10-04 22:55:40 +0300

    Solution Task 4

--- Task_4.py
+++ Task_4.py
@@ -1,1 +1,9 @@
 # 4. Show the first digit of the fractional part of the number.
+
+num = float(input('Enter a fractional number: '))
+
+# num = 5,77
+round_num = int(num) #5
+result = (num - round_num)*10 #7.7
+
+print(f'The first digit of the fractional part: {int(result)}')
