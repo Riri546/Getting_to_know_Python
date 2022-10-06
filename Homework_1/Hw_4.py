72e196395c3a7204edf88c12395a2f4ebb95a658
Лилия <l_loshakova123@mail.ru>
2022-10-06 16:57:32 +0300

    Solution homework 4

--- Homework_1/Hw_4.py
+++ Homework_1/Hw_4.py
@@ -1,24 +1,21 @@
 # Write a program that, based on a given number of quarters, 
 # shows the range of possible coordinates of points in this quarter (x and y).
 
+print('Enter the quarter number: ')
+number = int(input())
 
+# The method determines the ranges x and h by the number of the quarter
+def print_answer(num):
+    if num == 1:
+        print("Acceptable: x>0, y>0")
+    if num == 2:
+        print("Acceptable: x<0, y>0")
+    if num ==3:
+        print("Acceptable: x<0, y<0")
+    if num ==4:
+         print("Acceptable: x>0, y<0")
+    if num >= 5:
+        print("Error")
 
+print_answer(number)
 
-# Console.Clear();
-
-# void PrintAnswer(int number)
-# {
-#     if (number == 1) Console.WriteLine("Acceptable: x>0, y>0");
-#     if (number == 2) Console.WriteLine("Acceptable: x<0, y>0");
-#     if (number == 3) Console.WriteLine("Acceptable: x<0, y<0");
-#     if (number == 4) Console.WriteLine("Acceptable: x>0, y<0");
-# }
-
-# string? inputLine = Console.ReadLine();
-
-# if (inputLine != null)
-# {
-#     int inputNumber = int.Parse(inputLine);
-
-#     PrintAnswer(inputNumber);
-# }
