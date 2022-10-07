00a0e0857b0137f98c888dabf287433cb74befbf
Лилия <l_loshakova123@mail.ru>
2022-10-07 22:42:04 +0300

    Outline of the solution

--- Homework_2/Hw_1.py
+++ Homework_2/Hw_1.py
@@ -2,3 +2,11 @@
 # *Example:*
 # - 6782 -> 23
 # - 0,56 -> 11
+
+number = str(input('Enter a real number: '))
+
+result = 0
+
+for i in len(number):
+    result = number[i]+number[i+1]
+print('Answer {result}')
