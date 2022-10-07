0c03034ac42fe0964b9e9dec95b3838ab02eb9b8
Лилия <l_loshakova123@mail.ru>
2022-10-07 23:22:33 +0300

    Outline of the solution

--- Homework_2/Hw_2.py
+++ Homework_2/Hw_2.py
@@ -1,3 +1,16 @@
 # 2. Write a program that accepts the number N as input and outputs a set of products of numbers from 1 to N.
 # *Example:*
 # N = 4, then [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+number = int(input('Enter the natural number n: '))
+result = ''
+
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
+        
+
+print(factorial(number))
