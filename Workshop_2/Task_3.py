82786c53cecff11ce5a53571fa44532e1d1e976b
Лилия <l_loshakova123@mail.ru>
2022-10-07 22:02:03 +0300

    Solution Task 3

--- Workshop_2/Task_3.py
+++ Workshop_2/Task_3.py
@@ -1,2 +1,6 @@
 # 3. Write a program in which the user will set two lines, and the program
 # will determine the number of occurrences of one line in another.
+
+first_string = input('Enter the first line: ')
+second_string = input('Enter the second line: ')
+print(f'The number of occurrences of one line in the first: {first_string.count(second_string)}')
