0423ee9bff78daaba8a3ca3730a02358adf20071
Лилия <l_loshakova123@mail.ru>
2022-10-07 21:35:57 +0300

    Solution Task 1

--- Workshop_2/Task_1.py
+++ Workshop_2/Task_1.py
@@ -1,3 +1,13 @@
 # 1. Write a program that accepts the number N as input and outputs a sequence of N terms.
 # Example:
 # - For N = 5: 1, -3, 9, -27, 81
+
+n = int(input('Enter the natural number n: '))
+result = ''
+sigh = 1
+
+for i in range(0, n):
+    result += str(3 ** i * sigh)
+    result += ', '
+    sigh = - sigh
+print(result[0:(len(result) - 2)])
