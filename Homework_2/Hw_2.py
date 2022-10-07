65441a7119beeb14446aee4c1d75a8dc5432058d
Лилия <l_loshakova123@mail.ru>
2022-10-07 22:13:32 +0300

    Adding a homework 2

--- Homework_2/Hw_2.py
+++ Homework_2/Hw_2.py
@@ -1,1 +1,3 @@
-
+# 2. Write a program that accepts the number N as input and outputs a set of products of numbers from 1 to N.
+# *Example:*
+# - let N = 4, then [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
