a92326017b265dc32294094a55cc7fbeb5dc5dad
Лилия <l_loshakova123@mail.ru>
2022-10-07 22:14:24 +0300

    Adding a homework 3

---
+++ Homework_2/Hw_3.py
@@ -0,0 +1,5 @@
+# 3.Specify a list of n numbers in the sequence $(1+\frac 1 n)^n$ and display their sum on the screen.
+# *Example:*
+# - For n = 6:
+#     [2.0, 2.25, 2.3704, 2.4414, 2.4883, 2.5216]
+#     Sum of numbers = 14.0717
