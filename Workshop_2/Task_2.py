2e4f9cc2a556e75108a91a9b019410f384f1d8da
Лилия <l_loshakova123@mail.ru>
2022-10-07 21:55:42 +0300

    Solution Task 3

--- Workshop_2/Task_2.py
+++ Workshop_2/Task_2.py
@@ -2,11 +2,22 @@
 # Example:
 # - For n = 6: [4, 7, 10, 13, 16, 19]
 
-n = int(input('Enter the natural number n: '))
-result = ''
-sigh = 1
+from unittest import result
 
-for i in range(0, n+1):
-    result += str((3*i)+1)
-    result += ', '
-print(result[0:(len(result)-2)])
+
+n = int(input('Enter the natural number n: '))
+result = []
+for i in range(1, n +1):
+    result.append(3*i+1)
+print(result)
+print(*result, sep=', ')
+
+
+
+# result = ''
+# sigh = 1
+
+# for i in range(0, n+1):
+#     result += str((3*i)+1)
+#     result += ', '
+# print(result[0:(len(result)-2)])
