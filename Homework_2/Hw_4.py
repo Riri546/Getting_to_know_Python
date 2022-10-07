1aa8b93f91095a7e1d890158c65e0288c81376aa
Лилия <l_loshakova123@mail.ru>
2022-10-07 22:14:44 +0300

    Adding a homework 4

---
+++ Homework_2/Hw_4.py
@@ -0,0 +1,3 @@
+# 4.Specify a list of N elements filled with numbers from the interval [-N, N]. 
+# Find the product of the elements at the specified positions. The positions are stored 
+# in a file file.txt there is one number in one line.
