18aebf3b41e6ac9cb774c1079ec66e09e11fa132
Лилия <l_loshakova123@mail.ru>
2022-10-07 23:57:02 +0300

    Adding a task 4

---
+++ Seminar_3/Task_4.py
@@ -0,0 +1,3 @@
+# 4 Set a list of N elements filled with numbers from [-N, N]. Find the product of the elements
+# at the specified positions. Positions are stored in the positions list - create this list (for example:
+# positions = [1, 3, 6]).
