c50c5b1e6799bb9925cd90ed7c9c19bcf87542d3
Лилия <l_loshakova123@mail.ru>
2022-10-08 14:30:32 +0300

    Outline of the solution

--- Homework_2/Hw_3.py
+++ Homework_2/Hw_3.py
@@ -3,3 +3,25 @@
 # - For n = 6:
 #     [2.0, 2.25, 2.3704, 2.4414, 2.4883, 2.5216]
 #     Sum of numbers = 14.0717
+
+number = input('Enter number: ')
+sum_result = 0
+result = []
+
+
+def filling_list(n, res):
+    filling = 0
+    for i in range(0, n):
+        filling = ((1+(n/1))**n)
+        res.append(filling)
+    print(res)
+
+
+def sum_number(sum, n):
+    for i in n:
+        sum += i
+    print(f'Sum of numbers: {sum}')
+
+
+filling_list(number, result)
+sum_number(sum_result, number)
