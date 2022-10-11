29a419bd6fd72906181ece18dc4e2b793f8ef0e0
Лилия <l_loshakova123@mail.ru>
2022-10-11 17:49:57 +0300

    Outline of the solution

--- Homework_2/Hw_4.py
+++ Homework_2/Hw_4.py
@@ -1,3 +1,10 @@
-# 4.Specify a list of N elements filled with numbers from the interval [-N, N]. 
-# Find the product of the elements at the specified positions. The positions are stored 
+# 4.Specify a list of N elements filled with numbers from the interval [-N, N].
+# Find the product of the elements at the specified positions. The positions are stored
 # in a file file.txt there is one number in one line.
+
+n_neg = int(input('Enter a negative number: '))
+n_pos = int(input('Enter a positive number: '))
+
+list = [i for i in range(n_neg, n_pos+1)]
+print(list)
+print('Sum of numbers:', sum(list))
