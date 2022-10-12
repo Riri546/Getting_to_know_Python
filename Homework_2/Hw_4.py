6c8933ca36ebb89679bf3269024263b670bb5f9a
Лилия <l_loshakova123@mail.ru>
2022-10-12 18:24:52 +0300

    Solution  homework 4

--- Homework_2/Hw_4.py
+++ Homework_2/Hw_4.py
@@ -4,7 +4,22 @@
 
 n_neg = int(input('Enter a negative number: '))
 n_pos = int(input('Enter a positive number: '))
+position_1 = int(input('Enter the index of the first number: '))
+position_2 = int(input('Enter the index of the second number: '))
 
-list = [i for i in range(n_neg, n_pos+1)]
-print(list)
-print('Sum of numbers:', sum(list))
+list_num = [i for i in range(n_neg, n_pos+1)]
+print(list_num)
+
+
+def sum_number(l, pos_1, pos_2):
+    count = 0
+    for i in l:
+        if i == pos_1:
+            count += l[i]
+        if i == pos_2:
+            count += l[i]
+    print(
+        f'The sum of the numbers standing by the index {position_1} and {position_2} = {count}')
+
+
+sum_number(list_num, position_1, position_2)
