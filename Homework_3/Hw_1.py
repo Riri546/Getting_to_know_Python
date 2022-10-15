1f08d5525724776a9c32d1ba6bb5412aa50e48a5
Лилия <l_loshakova123@mail.ru>
2022-10-15 14:03:31 +0300

    Outline of the solution

--- Homework_3/Hw_1.py
+++ Homework_3/Hw_1.py
@@ -2,40 +2,19 @@
 # *Example:*
 # - [2, 3, 5, 9, 3] -> on the odd positions of elements 3 and 9, the answer is: 12
 
-filling = []
+int_list = [] 
+for element in input('Enter integers in the list separated by a space: ').split():
+    int_list.append(int(element))
+print(int_list)
 
-def filling_list(fil):
-    length = int(input('Enter the length of the list: '))
-    number = int(input(f'Enter {length} items in the list: '))
-    
+
+def sum_list(int_l):
+    length = len(int_list)
+    print(f'List length = {length}')
+    count = 0
     for i in length:
-        fil.append(number)
-    print(filling)
-    
-filling_list(filling)
-    
+        if i%2 != 0:
+            count += int_l(i)
+    print(f'Aswer: {count}')      
 
-
-# def sum_num(fil):
-#     count = 0
-#     if fil % 2 != 0:
-#         count += fil
-#     print(f'Answer = {count}')
-
-
-# sum_num(filling)
-
-# number = int(input("Enter the number of the day of the week: "))
-
-# # Checking the condition
-# def days(num):
-#     week = ['Monday', 'Tuesday', 'Wednesday',
-#             'Thursday', 'Friday', 'Saturday', 'Sunday']
-#     if num < 1 or num > 7:
-#         print('You entered an incorrect value!')
-#     elif num >= 6:
-#         print(f"{week[num-1]} It's a day off")
-#     else:
-#         print(f"{week[num-1]} It's a working day")
-
-# days(number)
+sum_list(int_list)
