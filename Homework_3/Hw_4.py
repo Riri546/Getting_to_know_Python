d45f79d3dc9473f94535634d74a38a464973b7d9
Лилия <l_loshakova123@mail.ru>
2022-10-16 17:25:25 +0300

    Outline of the solution

--- Homework_3/Hw_4.py
+++ Homework_3/Hw_4.py
@@ -2,3 +2,19 @@
 
 number = input('Enter number: ')
 
+
+def add_binary(a):
+    ostatok = a % 2
+    delimoe = a//2
+    answer = [ostatok]
+    while (delimoe > 0):
+        ostatok = delimoe % 2
+        delimoe = delimoe//2
+        answer.append(ostatok)
+    answer.reverse()
+    answerstring = ''.join(str(e) for e in answer)
+    print(answerstring)
+    return (answerstring)
+
+
+print(add_binary(number))
