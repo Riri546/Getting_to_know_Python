a5221ac321efb670221ff83aca1f09f564158663
Лилия <l_loshakova123@mail.ru>
2022-10-18 00:26:19 +0300

    Solution  task 1

--- Homework_3/Hw_1.py
+++ Homework_3/Hw_1.py
@@ -1,8 +1,6 @@
 # 1.Specify a list of several numbers. Write a program that will find the sum of the list items standing in an odd position.
 # *Example:*
-# - [2, 3, 5, 9, 3] -> on the odd positions of elements 3 and 9, the answer is: 12
-
-from itertools import count
+# - [2, 3, 5, 8, 3, 1] -> on the odd positions of elements 3, 9 and 1 the answer is: 12
 
 
 int_list = []
@@ -14,13 +12,11 @@ print(int_list)
 def sum_list(int_l):
     length = len(int_l)
     print(f'List length = {length}')
-    print(type(length))
-    count = 0
-    for i in range(length+1):
+    sum_odd = 0
+    for i in range(0, length + 1):
         if i % 2 != 0:
-            count += i
-    print(f'Answer: {count}')
-    return count
+            sum_odd += int_l[i]
+    print(f'The sum of all the odd numbers in the list: {sum_odd}')
 
 
 sum_list(int_list)
