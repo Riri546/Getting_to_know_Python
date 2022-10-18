c34bf423591532a05cbe7929211276c2ecbdc462
Лилия <l_loshakova123@mail.ru>
2022-10-18 13:49:46 +0300

    Outline of the solution

--- Seminar_3/Homework_3/Hw_5.py
+++ Seminar_3/Homework_3/Hw_5.py
@@ -3,19 +3,22 @@
 # - for k = 8 , the list will look like this: [-21 ,13, -8, 5, -3, 2, -1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 # [ Negafibonacci] (https://ru.wikipedia.org/wiki/Негафибоначчи )
 
-fib1 = fib2 = 1
-
-fib = []
-
-n = int(input('Enter number: '))
 
 
-print(fib1, fib2, end=' ')
 
-for i in range(2, n):
-    fib1, fib2 = fib2, fib1 + fib2
-    print(fib2, end = ' ')
+# fib1 = fib2 = 1
 
-for i in range(2, n):
-    fib1, fib2 = fib2, fib1 + fib2
-    print(fib2, end = ' ')
+# fib = []
+
+# n = int(input('Enter number: '))
+
+
+# print(fib1, fib2, end=' ')
+
+# for i in range(2, n):
+#     fib1, fib2 = fib2, fib1 + fib2
+#     print(fib2, end = ' ')
+
+# for i in range(2, n):
+#     fib1, fib2 = fib2, fib1 + fib2
+#     print(fib2, end = ' ')
