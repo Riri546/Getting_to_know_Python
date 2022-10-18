fad97f4d331b60fd75b885e35b2cb9a8bf2abf36
Лилия <l_loshakova123@mail.ru>
2022-10-18 13:18:57 +0300

    Solution  task 3

--- Seminar_3/Homework_3/Hw_3.py
+++ Seminar_3/Homework_3/Hw_3.py
@@ -1,4 +1,4 @@
-# 3.Specify a list of real numbers. Write a program that will find the difference between the maximum 
+# 3.Specify a list of real numbers. Write a program that will find the difference between the maximum
 # and minimum values of the fractional part of the elements.
 # *Example:*
 # - [1.1, 1.2, 3.1, 5, 10.01] => 0.19
@@ -6,8 +6,15 @@
 source_list = [1.1, 1.2, 3.1, 5, 10.01]
 print(source_list)
 
-print(f'Maximum value of the list: {max(source_list)}')
-print(f'Maximum value of the list: {min(source_list)}')
 
-print(f'The difference between the maximum and minimum values: {max(source_list)-min(source_list)}')
-    
+def max_min(max_l, min_l, s_l):
+    for i in s_l:
+        if i - int(i) >= max_l:
+            max_l = i - int(i)
+        if i - int(i) <= min_l:
+            min_l = i - int(i)
+    print(
+        f'The difference between the maximum and minimum values: {max_l - min_l}')
+
+
+max_min(0, 1, source_list)
