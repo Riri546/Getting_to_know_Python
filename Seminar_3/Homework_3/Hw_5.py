ce30ff00f429f1ab52d91709901467c2985b19d2
Лилия <l_loshakova123@mail.ru>
2022-10-18 22:08:09 +0300

    Outline of the solution

--- Seminar_3/Homework_3/Hw_5.py
+++ Seminar_3/Homework_3/Hw_5.py
@@ -2,21 +2,30 @@
 # *Example:*
 # - for k = 8 , the list will look like this: [-21 ,13, -8, 5, -3, 2, -1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 # [ Negafibonacci] (https://ru.wikipedia.org/wiki/Негафибоначчи )
+num = int(input('Enter num: '))
+list_f = []
 
 
-fib1 = fib2 = 1
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n in [1, 2]:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+        
+    
 
-fib = []
 
-n = int(input('Enter number: '))
+def negafibonacci(n):
+
+    if n == 1:
+        return 1
+    if n == 2:
+        return -1
+    else:
+        return fibonacci(n+1) - fibonacci(n+2)
 
 
-print(fib1, fib2, end=' ')
-
-for i in range(2, n):
-    fib1, fib2 = fib2, fib1 + fib2
-    print(fib2, end = ' ')
-
-for i in range(2, n):
-    fib1, fib2 = fib2, fib1 + fib2
-    print(fib2, end = ' ')
+print(fibonacci(num))
+print(negafibonacci(num))
