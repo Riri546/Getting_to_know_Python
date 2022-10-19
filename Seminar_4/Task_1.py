e698edef1397d8a7f7091a78eccbafa7b84c310c
Лилия <l_loshakova123@mail.ru>
2022-10-19 13:56:17 +0300

    New file

---
+++ Seminar_4/Task_1.py
@@ -0,0 +1,10 @@
+# 1. Specify a string from a set of numbers. Write a program that will show a larger and
+# smaller number. Use a space as the separator character.
+
+us_list = []
+for element in input('Enter integers in the list separated by a space: ').split():
+    us_list.append(element)
+print(us_list)
+
+print(f'Min: {min(us_list)}')
+print(f'Max: {max(us_list)}')
