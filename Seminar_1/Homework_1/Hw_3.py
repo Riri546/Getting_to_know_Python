e3141809bb5c51da1c22c90d96badb5a2f611ac7
Лилия <l_loshakova123@mail.ru>
2022-10-28 22:04:54 +0300

    Recoding

--- Seminar_1/Homework_1/Hw_3.py
+++ Seminar_1/Homework_1/Hw_3.py
@@ -1,18 +1,19 @@
 # Write a program that takes the coordinates of a point (X and Y) as input, with X ≠ 0 and Y ≠ 0 and
 #  outputs the number of the quarter of the plane in which this point is located (or on which axis it is located).
 
-print('Enter the x coordinate: ')
-number_x = int(input())
-print('Enter the y coordinate: ')
-number_y = int(input())
+
+number_x = int(input('Enter the x coordinate: '))
+number_y = int(input('Enter the y coordinate: '))
 
 # Checking the condition
+
+
 def checking_condition(x, y):
     if x != 0 and y != 0:
         if x > 0 and y > 0:
             print('This point is in 1 quarter')
         if x < 0 and y > 0:
-         print('This point is in 2 quarter')
+            print('This point is in 2 quarter')
         if x < 0 and y < 0:
             print('This point is in 3 quarter')
         if x > 0 and y < 0:
