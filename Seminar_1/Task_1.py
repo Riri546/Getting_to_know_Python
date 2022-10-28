0d9090c3939dbc1b96e102368cafd7cdf437f156
Лилия <l_loshakova123@mail.ru>
2022-10-28 21:38:08 +0300

    Recoding

--- Seminar_1/Task_1.py
+++ Seminar_1/Task_1.py
@@ -8,3 +8,6 @@ if a == b ** 2:
     print(f'The number {a}  is the square of {b}')
 else:
     print(f'The number {a}  is not the square of {b}!')
+
+
+
--- Seminar_1/Task_2.py
+++ Seminar_1/Task_2.py
@@ -1,41 +1,41 @@
 # 2. Find the maximum of five numbers.
 
-print('Enter the number a: ')
-a = int(input())
-print('Enter the number b: ')
-b = int(input())
-print('Enter the number c: ')
-c = int(input())
-print('Enter the number d: ')
-d = int(input())
-print('Enter the number e: ')
-e = int(input())
 
-max = 0
+a = int(input('Enter the number a: '))
+b = int(input('Enter the number b: '))
+c = int(input('Enter the number c: '))
+d = int(input('Enter the number d: '))
+e = int(input('Enter the number e: '))
 
-if (max > a):
-    max
-else:
-    max = a
 
-if (max > b):
-    max
-else:
-    max = b
+list_user = [a, b, c, d, e]
+print(max(list_user))
 
-if (max > c):
-    max
-else:
-    max = c
+# max = 0
 
-if (max > d):
-    max
-else:
-    max = d
+# if (max > a):
+#     max
+# else:
+#     max = a
 
-if (max > e):
-    max
-else:
-    max = e
+# if (max > b):
+#     max
+# else:
+#     max = b
 
-print(f'Max = {max}')
+# if (max > c):
+#     max
+# else:
+#     max = c
+
+# if (max > d):
+#     max
+# else:
+#     max = d
+
+# if (max > e):
+#     max
+# else:
+#     max = e
+
+# print(f'Max = {max}')
