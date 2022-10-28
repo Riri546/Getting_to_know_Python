4bf5c0bfc2f2dbb8433bb9353c096887e3fe1e50
Лилия <l_loshakova123@mail.ru>
2022-10-28 22:04:18 +0300

    Recoding

--- Seminar_1/Homework_1/Hw_2.py
+++ Seminar_1/Homework_1/Hw_2.py
@@ -1,12 +1,10 @@
 # Write a program for. verification of the truth of the statement 
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z  for all values of the predicate.
 
-print('Enter the number x: ')
-input_x = int(input())
-print('Enter the number y: ')
-input_y = int(input())
-print('Enter the number z: ')
-input_z = int(input())
+
+input_x = int(input('Enter the number x: '))
+input_y = int(input('Enter the number y: '))
+input_z = int(input('Enter the number z: '))
 
 # Checking the condition
 def verification(x, y, z):
