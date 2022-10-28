1cb64dabce4c9a88f28c9fa2952d5ddfa7548de4
Лилия <l_loshakova123@mail.ru>
2022-10-28 22:00:16 +0300

    Recoding

--- Seminar_1/Task_10.py
+++ Seminar_1/Task_10.py
@@ -2,19 +2,16 @@
 
 import math
 
-print('Enter the coordinates of point a on the x axis: ')
-coord_x_point_a = int(input())
-print('Enter the coordinates of point b on the x axis:: ')
-coord_x_point_b = int(input())
-print('Enter the coordinates of point a on the y axis: ')
-coord_y_point_a = int(input())
-print('Enter the coordinates of point a on the y axis:: ')
-coord_y_point_b = int(input())
+coord_x_point_a = int(input('Enter the coordinates of point a on the x axis: '))
+coord_x_point_b = int(input('Enter the coordinates of point b on the x axis: '))
+coord_y_point_a = int(input('Enter the coordinates of point a on the y axis: '))
+coord_y_point_b = int(input('Enter the coordinates of point a on the y axis: '))
 
-length_A_B = 0
 
 def Calculate(x_a, x_b, y_a, y_b, length):
-    length = math.sqrt((math.pow((x_a - x_b), 2) + math.pow((y_a  - y_b ), 2)))
-    print('Answer: ', length)
+    length = math.sqrt((math.pow((x_a - x_b), 2) + math.pow((y_a - y_b), 2)))
+    print(f'Answer: {length}')
 
-Calculate(coord_x_point_a, coord_x_point_b,coord_y_point_a,coord_y_point_b, length_A_B)
+
+Calculate(coord_x_point_a, coord_x_point_b,
+          coord_y_point_a, coord_y_point_b,0)
