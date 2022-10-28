587ce5c47b4da5a7004d2e458b8d55f4c9d2fbcf
Лилия <l_loshakova123@mail.ru>
2022-10-28 18:14:34 +0300

    Comment with the task

---
+++ Seminar_5/Task_2.py
@@ -0,0 +1,4 @@
+# 2ю Dan listok chisel. Find a list, in Kotor Urgench find numbers, describe Urgench
+# growing consistency. Order of transgendered not change.
+# Example:
+# [1, 5, 2, 3, 4, 6, 1, 7] => [1, 2, 3] either [1, 7] or [1, 6, 7]etc.D.
