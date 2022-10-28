537e635fb112dcc7cf69244434ecd555f16c2000
Лилия <l_loshakova123@mail.ru>
2022-10-28 21:43:08 +0300

    Recoding

--- Seminar_1/Task_2_var_2.py
+++ Seminar_1/Task_2_var_2.py
@@ -1,17 +1,17 @@
 # 2. Find the maximum of five numbers.
 
 a = [5, 66, 33, 54, 11]
+print(f'Answer: {max(a)}')
+
+# def find_max_value(list):
+#     index = 0
+#     max_index = len(list)-1
+#     max = list[0]
+#     while index <= max_index:
+#         if list[index] > max:
+#             max = list[index]
+#         index += 1
+#     return max
 
 
-def find_max_value(list):
-    index = 0
-    max_index = len(list)-1
-    max = list[0]
-    while index <= max_index:
-        if list[index] > max:
-            max = list[index]
-        index += 1
-    return max
-
-
-print('Answer: ', find_max_value(a))
+# print('Answer: ', find_max_value(a))
