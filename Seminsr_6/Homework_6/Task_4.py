2204a7888b21cb9b7e0774129c656cf7c0e831fb
Лилия <l_loshakova123@mail.ru>
2022-10-30 13:43:53 +0300

    New file for task 4

---
+++ Seminsr_6/Homework_6/Task_4.py
@@ -0,0 +1,22 @@
+# 3. Specify two numbers. Write a program that will find the NOC (smallest common
+# multiple) of these two numbers.
+
+
+x = int(input('Enter the number x: '))
+y = int(input('Enter the number y: '))
+
+
+def nod(a, b):
+    if b == 0:
+        return a
+    else:
+        return nod(b, a % b)
+
+nok = lambda a, b: abs(a * b) / nod(a, b)
+print(f'NOC = {nok(x, y)}')
+# def nok(a, b):
+#     return int(abs(a * b) / nod(a, b))
+
+
+print(f'NOD = {nod(x, y)}')
+# print(f'NOC = {nok(x, y)}')
