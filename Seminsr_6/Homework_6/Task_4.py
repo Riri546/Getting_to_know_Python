406a1db499cd81cd7c55b24d21baf87bc07634a6
Лилия <l_loshakova123@mail.ru>
2022-10-30 13:46:42 +0300

    Editing

--- Seminsr_6/Homework_6/Task_4.py
+++ Seminsr_6/Homework_6/Task_4.py
@@ -2,6 +2,9 @@
 # multiple) of these two numbers.
 
 
+from http.client import NETWORK_AUTHENTICATION_REQUIRED
+
+
 x = int(input('Enter the number x: '))
 y = int(input('Enter the number y: '))
 
@@ -11,12 +14,14 @@ def nod(a, b):
         return a
     else:
         return nod(b, a % b)
+print(f'NOD = {nod(x, y)}')
 
-nok = lambda a, b: abs(a * b) / nod(a, b)
-print(f'NOC = {nok(x, y)}')
+
+noki = lambda a, b: int(abs(a * b) / nod(a, b))
+print(f'NOC = {noki(x, y)}')
+
 # def nok(a, b):
 #     return int(abs(a * b) / nod(a, b))
-
-
-print(f'NOD = {nod(x, y)}')
 # print(f'NOC = {nok(x, y)}')
+
+
