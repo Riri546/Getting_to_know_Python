7674c9e0845446bdad9b06a13523c4ad6cdcfd83
Лилия <l_loshakova123@mail.ru>
2022-11-01 14:33:19 +0300

    Editing

--- Lecture_4/Task_2/logger.py
+++ Lecture_4/Task_2/logger.py
@@ -4,19 +4,19 @@ from datetime import datetime as dt
 def temperature_logger(data):
     time = dt.now().strftime('%H:%M')
     with open('log.csv', 'a') as file:
-        file.write('{}; temperature; {}'
+        file.write('{}; temperature; {}\n'
                    .format(time, data))
 
 
 def pressure_logger(data):
     time = dt.now().strftime('%H:%M')
     with open('log.csv', 'a') as file:
-        file.write('{}; pressure; {}'
+        file.write('{}; pressure; {}\n'
                    .format(time, data))
 
 
 def wind_speed_logger(data):
     time = dt.now().strftime('%H:%M')
     with open('log.csv', 'a') as file:
-        file.write('{}; wind speed; {}'
+        file.write('{}; wind speed; {}\n'
                    .format(time, data))
