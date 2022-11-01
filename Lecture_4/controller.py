5290f8b64b20b764fb364c651c51287202e5d704
Лилия <l_loshakova123@mail.ru>
2022-11-01 13:18:45 +0300

    Code outline

--- Lecture_4/controller.py
+++ Lecture_4/controller.py
@@ -1,10 +1,10 @@
 from unittest import result
-import model
+import model_mult as model
 import view
 
 def button_click():
     value_a = view.get_value()
     value_b = view.get_value()
     model.init(value_a, value_b)
-    result = model.sum()
+    result = model.mult()
     view.view_data(result)
