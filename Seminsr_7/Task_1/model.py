57fdb83cc9a4e154e27f86b7dbcd911069d4e104
Лилия <l_loshakova123@mail.ru>
2022-11-02 01:09:36 +0300

    Code outline

---
+++ Seminsr_7/Task_1/model.py
@@ -0,0 +1,19 @@
+def sum_num(x, y):
+    return x + y
+
+
+def sub_num(x, y):
+    return x - y
+
+
+def mult_num(x, y):
+    return x * y
+
+
+def div_num(x, y):
+    return x / y
+
+
+x = complex(5, 5)
+y = complex(5, 5)
+print(sum_num(x, y))
