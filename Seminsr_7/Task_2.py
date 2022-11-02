d8319131ed19decaeba1e6caa9b43e6a8fb249bd
Лилия <l_loshakova123@mail.ru>
2022-11-02 23:10:45 +0300

    Code outline

--- Seminsr_7/Task_2.py
+++ Seminsr_7/Task_2.py
@@ -1,2 +1,20 @@
-def summ(x,y):
-    
+import pprint
+
+# Пример того, как нужно описывать функцию
+
+# Функция суммирует жва числа
+def summ(x: int, y: int) -> float:
+    """
+    Functional get ywo integer numbers and return sum 
+    :param x: int
+    :param y: int
+    :return: float
+    """
+    return float(x + y)
+
+
+summ(5, 2)
+lst = [2, 3, 5]
+sum(lst)
+# lst2 = ['2', 5, [3, 2]]
+# sum(lst2)
