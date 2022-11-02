8533bb3b0fb49165ab3c892d1b0768edf0aae78b
Лилия <l_loshakova123@mail.ru>
2022-11-03 02:13:09 +0300

    Editing

--- Seminsr_7/Homework_7/controller.py
+++ Seminsr_7/Homework_7/controller.py
@@ -1,4 +1,5 @@
 import logger as log
+import json_logger as jl
 import data_provider as dp
 
 
@@ -8,8 +9,13 @@ def button_click():
     name = dp.name_use()
     tel = dp.telephon_use()
     dasc = dp.description_use()
+
     log.surname_logger(surname)
     log.name_logger(name)
     log.telephon_logger(tel)
     log.description_logger(dasc)
     
+    # jl.surname_json(surname)
+    # jl.name_json(name)
+    # jl.telephon_json(tel)
+    # jl.description_json(dasc)
--- Seminsr_7/Homework_7/main.py
+++ Seminsr_7/Homework_7/main.py
@@ -1,5 +1,7 @@
 import controller as c
+import xml_generstor as xg
 
+print(xg.create())
 
 c.button_click()
 
