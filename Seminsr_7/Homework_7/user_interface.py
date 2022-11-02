1f16d8609309a41503646cc2b03427106d45cb46
Лилия <l_loshakova123@mail.ru>
2022-11-03 00:12:54 +0300

    Code outline

--- Seminsr_7/Homework_7/user_interface.py
+++ Seminsr_7/Homework_7/user_interface.py
@@ -1,9 +1,29 @@
-from importlib import import_module
 import data_provider as dp
 import logger as log
 
-def surname_view():
-    data = dp.surname_use()
+
+def surname_view(fam):
+    data = dp.surname_use(fam)
+    log.surname_logger(data)
+    return data
+
+
+def name_view(name):
+    data = dp.name_use(name)
+    log.name_logger(data)
+    return data
+
+
+def telephon_view(tel):
+    data = dp.telephon_use(tel)
+    log.telephon_logger(data)
+    return data
+
+
+def description_view(desc):
+    data = dp.description_use(desc)
+    log.description_logger(data)
+    return data
 
 
 # def temperature_view(sensor):
