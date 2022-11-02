4398f5a02b0f761ec241357c9eefbd2104335918
Лилия <l_loshakova123@mail.ru>
2022-11-03 00:34:00 +0300

    Code outline

---
+++ Seminsr_7/Homework_7/controller.py
@@ -0,0 +1,25 @@
+import logger as log
+import data_provider as dp
+
+
+def button_click():
+    surname = dp.surname_use()
+    name = dp.name_use()
+    tel = dp.telephon_use()
+    dasc = dp.description_use()
+
+
+    # value_a = view.get_value(1)
+    # value_b = view.get_value(2)
+    # operator = view.get_operation()
+    # if operator == '+':
+    #     result = mod.sum_num(value_a, value_b)
+    # elif operator == '-':
+    #     result = mod.sub_num(value_a, value_b)
+    # elif operator == '/':
+    #     result = mod.div_num(value_a, value_b)
+    # elif operator == '*':
+    #     result = mod.mult_num(value_a, value_b)
+    # op = f'{value_a} {operator} {value_b}'
+    # view.view_data(op, result)
+    # log.operation_logger(op, result)
