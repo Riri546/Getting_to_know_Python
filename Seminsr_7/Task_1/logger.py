4178c75b3d42d381039b658fc4e7441047340d0e
Лилия <l_loshakova123@mail.ru>
2022-11-02 22:18:48 +0300

    Editing

--- Seminsr_7/Task_1/logger.py
+++ Seminsr_7/Task_1/logger.py
@@ -3,4 +3,4 @@ from datetime import datetime as dt
 def operation_logger(op, result):
     time = dt.now().strftime('%D - %H:%M')
     with open('log_complex.csv', 'a') as file:
-        file.write(f'{op} = {result}\n')
+        file.write(f'{time}: {op} = {result}\n')
