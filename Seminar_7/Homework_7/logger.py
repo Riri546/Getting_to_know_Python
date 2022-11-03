d8de7f3583bfd1afcc21936bcc10a59159777a6f
Лилия <l_loshakova123@mail.ru>
2022-11-03 12:56:51 +0300

    Editing

--- Seminar_7/Homework_7/logger.py
+++ Seminar_7/Homework_7/logger.py
@@ -1,18 +1,18 @@
 def surname_logger(data):
-    with open('log_telephone.csv', 'a') as file:
+    with open('log_telephon.csv', 'a') as file:
         file.write(f'Surname: {data}\n')
 
 
 def name_logger(data):
-    with open('log_telephone.csv', 'a') as file:
+    with open('log_telephon.csv', 'a') as file:
         file.write(f'Name: {data}\n')
 
 
 def telephon_logger(data):
-    with open('log_telephone.csv', 'a') as file:
+    with open('log_telephon.csv', 'a') as file:
         file.write(f'Telephon: {data}\n')
 
 
 def description_logger(data):
-    with open('log_telephone.csv', 'a') as file:
+    with open('log_telephon.csv', 'a') as file:
         file.write(f'Description: {data}\n''\n')
