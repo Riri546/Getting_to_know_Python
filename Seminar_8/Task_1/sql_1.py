7816ef6ea81565c9beed716ef1785b4d6e79a7e3
Лилия <l_loshakova123@mail.ru>
2022-11-03 03:09:21 +0300

    Editing

--- Seminar_8/Task_1/sql_1.py
+++ Seminar_8/Task_1/sql_1.py
@@ -5,6 +5,7 @@ bd = sqlite3.connect("Data_base.db")
 cursor = bd.cursor()
 
 cursor.execute('''CREATE TABLE IF NOT EXISTS personal(
+    id INT,
     name TEXT,
     last_name TEXT,
     position TEXT,
@@ -12,14 +13,19 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS personal(
     bonus INT
 )''')
 
-baza = [('Иван', 'Иванов', 'главный инженер', 500000, 10000),
-('Вадим', 'Корпатый', 'инженер', 551100, 2300),
-('Грегорий', 'Саломохин', 'бухгалтер', 230000, 100)]
+baza = [(1, 'Иван', 'Иванов', 'главный инженер', 500000, 10000),
+(2, 'Вадим', 'Корпатый', 'инженер', 551100, 2300),
+(3, 'Грегорий', 'Саломохин', 'бухгалтер', 230000, 100)]
 
-cursor.executemany('INSERT INTO personal VALUES(?,?,?,?,?)',baza)
+cursor.executemany('INSERT INTO personal VALUES(?,?,?,?,?,?)',baza)
 bd.commit()
 
 for i in cursor.execute('SELECT * FROM personal'):
     print(*i)
 
 cursor.execute('DELETE from personal WHERE')
+
+for i in cursor.execute('SELECT * FROM personal'):
+    print(*i)
+
+cursor.execute('DELETE from personal WRETE id=1')
