acd48a298f9ea39deba4928073b093aa9d41ebda
Лилия <l_loshakova123@mail.ru>
2022-11-04 22:40:04 +0300

    Code outline

---
+++ Seminar_8/Homework_8/data_provider.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+
+bd = sqlite3.connect("Data_base_HW.db")
+
+
+cursor = bd.cursor()
+
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS personal(
+    id INTEGER PRIMARY KEY autoincrement,
+    name TEXT,
+    last_name TEXT,
+    position TEXT,
+    salary INT,
+    bonus INT
+)''')
+
+
+baza = [(1, 'Иван', 'Иванов', 'главный инженер', 500000, 10000),
+        (2, 'Вадим', 'Корпатый', 'инженер', 551100, 2300),
+        (3, 'Грегорий', 'Саломохин', 'бухгалтер', 230000, 100)]
+
+try:
+    cursor.executemany('INSERT INTO personal VALUES(?,?,?,?,?,?)', baza)
+    bd.commit()
+except:
+    print('Данные уже есть')
